469b6d7372d0eba85a36b2075fd44cf2334beee9
pf <pf0214@qq.com>
2016-11-21 17:01:00 +0800

    Upload iOS Client with Objective-C

---
+++ GSCiOS_oc/GithubSocialClientOC/GithubSocialClientOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubSocialClientOC
+//
+//  Created by pf on 2016/11/21.
+//  Copyright © 2016年 fps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GSCiOS_oc/GithubSocialClientOC/GithubSocialClientOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubSocialClientOC
+//
+//  Created by pf on 2016/11/21.
+//  Copyright © 2016年 fps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
